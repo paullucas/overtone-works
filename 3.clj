b9127755a7d6877ae4a0cc34df055e7040a933f8
Paul <paulll.lucas@gmail.com>
2016-06-14 19:33:39 -0700

    Fix 3

--- 3.clj
+++ 3.clj
@@ -8,22 +8,22 @@
 (def three (get-sample "3"))
 (def four (get-sample "4"))
 
-(definst wrpr [buf 0 pointer 0 amp 1 att 10 mix 1 room 1 damp 1 gate 1 freq-scale 1 window-size 0.1 envbufnum -1 overlaps 8 interp 1]
+(definst wrpr [buf 0 pointer 0 amp 1 att 15 mix 1 room 1 damp 1 gate 1 freq-scale 1 window-size 0.1 envbufnum -1 overlaps 1 interp 4]
  (let [src(* (free-verb :in (warp1 :num-channels 1 :bufnum buf :pointer pointer :freq-scale freq-scale :window-size window-size 
                                    :envbufnum envbufnum :overlaps overlaps :window-rand-ratio 0.0 :interp interp) :mix mix :room room :damp damp)
-             (env-gen (asr :attack att :curve 1 :release 20) :gate gate :action FREE))]
+             (env-gen (asr :attack att :curve 1 :release 40) :gate gate :action FREE))]
      (* src amp)))
 
-(definst wrpf [buf 0 pointer 0 amp 1 att 10 lff 2000 gate 1 freq-scale 1 window-size 0.1 envbufnum -1 overlaps 8 interp 1]
+(definst wrpf [buf 0 pointer 0 amp 1 att 15 lff 2000 gate 1 freq-scale 1 window-size 0.1 envbufnum -1 overlaps 1 interp 4]
  (let [src(* (lpf :in (warp1 :num-channels 1 :bufnum buf :pointer pointer :freq-scale freq-scale :window-size window-size 
                                    :envbufnum envbufnum :overlaps overlaps :window-rand-ratio 0.0 :interp interp) :freq lff)
-             (env-gen (asr :attack att :curve 1 :release 20) :gate gate :action FREE))]
+             (env-gen (asr :attack att :curve 1 :release 40) :gate gate :action FREE))]
      (* src amp)))
 
-(wrpr one :pointer 0.83 :freq-scale 0.4 :window-size 10.0 :overlaps 1 :interp 4 :amp 1.5 :attack 15 :release 40)  ;;42
-(wrpf three :lff 1000 :pointer 0.23 :freq-scale 0.8 :window-size 16.0 :overlaps 1 :interp 4 :amp 1.8 :attack 15 :release 40)   ;;43
-(wrpr two :pointer 0.27 :freq-scale 0.55 :window-size 9.0 :overlaps 1 :interp 4 :amp 0.7 :attack 15 :release 40) ;;44
-(wrpf four :lff 2000 :pointer 0.27 :freq-scale 2.3 :window-size 12.0 :overlaps 1 :interp 4 :amp 1.9 :attack 15 :release 40)  ;;44
+(wrpr one :pointer 0.83 :freq-scale 0.4 :window-size 10.0 :amp 1.5)               ;;42
+(wrpf three :lff 1000 :pointer 0.23 :freq-scale 0.8 :window-size 16.0 :amp 1.8)   ;;43
+(wrpr two :pointer 0.27 :freq-scale 0.55 :window-size 9.0 :amp 0.7)               ;;44
+(wrpf four :lff 2000 :pointer 0.27 :freq-scale 2.3 :window-size 12.0 :amp 1.9)    ;;45
 
 (ctl 42 :gate 0)
 (ctl 44 :gate 0)
@@ -35,7 +35,7 @@
 (ctl 43 :freq-scale 2.0)
 (ctl 45 :freq-scale 1.2)
 (ctl 43 :freq-scale 2.3)
-(wrpr two :pointer 0.67 :freq-scale 9.55 :window-size 3.0 :overlaps 1 :interp 4 :amp 0.7 :attack 5 :release 20) ;;46
+(wrpr two :pointer 0.67 :freq-scale 9.55 :window-size 3.0 :amp 0.7)               ;;46
 (ctl 46 :freq-scale 1.2 :window-size 11.4)
 (ctl 46 :freq-scale 0.55 :window-size 14 :amp 0.8)
 (ctl 46 :gate 0)
