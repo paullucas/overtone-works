5223e3b2b40576a4feeabbcde59396f82b6b5d4f
Paul <paulll.lucas@gmail.com>
2016-09-30 21:02:06 -0700

    Begin refactoring

--- src/otworks/c5.clj
+++ src/otworks/c5.clj
@@ -1,28 +1,6 @@
-(defn get-samples [folder-path filenames]
-  (doseq [filename filenames]
-    (intern *ns* (symbol filename) (load-sample (str folder-path filename ".wav")))))
-
-(def ugen-template
-  "(definst ugen-name [buf 0 pointer 0 amp 1 att 15 lff 2000 hff 200 mix 1 room 1 damp 1 gate 1 rate 1 freq 440 start-pos 0.0 freq-scale 1 window-size 0.1 envbufnum -1 overlaps 1 interp 4] (let [src (* ugen-input (env-gen (asr :attack att :curve 1 :release 40) :gate gate :action FREE))] (* src amp)))")
-
-(defn get-ugen [x]
-  (case x
-    "warp1" "(warp1 :num-channels 1 :bufnum buf :pointer pointer :freq-scale freq-scale :window-size window-size :envbufnum envbufnum :overlaps overlaps :window-rand-ratio 0.0 :interp interp)"
-    "warp1-lfo" "(warp1 :num-channels 1 :bufnum buf :pointer (+ 0.4 (* 0.1 (sin-osc:kr pointer))) :freq-scale freq-scale :window-size window-size :envbufnum envbufnum :overlaps overlaps :window-rand-ratio 0.0 :interp interp)"
-    "free-verb" "(free-verb :in ugen-input :mix mix :room room :damp damp)"
-    "lpf" "(lpf :in ugen-input :freq lff)"
-    "hpf" "(hpf :in ugen-input :freq hff)"))
-
-(defn gen-inst
-  ([name ugens]
-   (gen-inst name (drop-last ugens) (get-ugen (last ugens))))
-  ([name ugens result]
-   (if (= (count ugens) 1)
-     (let [ugen-out (clojure.string/replace (get-ugen (first ugens)) #"ugen-input" result)
-           ugen-final (clojure.string/replace (clojure.string/replace ugen-template #"ugen-input" ugen-out) #"ugen-name" name)]
-       (eval (read-string ugen-final)))
-     (let [ugen-out (clojure.string/replace (get-ugen (last ugens)) #"ugen-input" result)]
-       (gen-inst name (drop-last ugens) ugen-out)))))
+(ns otworks.c5
+  (:use overtone.live)
+  (:require [otworks.functions :refer [get-samples gen-inst]]))
 
 (gen-inst "wrpr" ["free-verb" "warp1"])
 (gen-inst "wrplf" ["lpf" "warp1"])
@@ -31,6 +9,7 @@
 (gen-inst "wrprlfo" ["free-verb" "lpf" "warp1-lfo"])
 
 (get-samples "~/Producing/july3rd-2016/samples/" ["smp1" "smp2" "smp3" "smp4"])
+(get-samples "~/Producing/june17th-2016/" ["s1" "s2" "s3"])
 
 (def wrplf1 (wrplf smp1 :pointer 0.83 :freq-scale 0.4 :window-size 14 :amp 1.5 :lff 3000))
 (def wrplf2 (wrprlf smp2 :pointer 0.1 :freq-scale 0.8 :window-size 8 :amp 1.1 :lff 5000))
@@ -69,8 +48,6 @@
 (ctl wrphf1 :hff 100)
 
 ;;
-
-(get-samples "~/Producing/june17th-2016/" ["s1" "s2" "s3"])
 
 (def wrplf1 (wrplf s1 :pointer 0.83 :freq-scale 0.4 :window-size 10 :amp 1.5 :lff 4000))
 (def wrpr1 (wrpr s2 :pointer 0.27 :freq-scale 0.5 :window-size 9 :amp 1.2))
