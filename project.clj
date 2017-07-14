aff7860c4529cf5611e2a3c9ec30652e835656f0
Paul <paulll.lucas@gmail.com>
2017-07-13 18:56:07 -0700

    Add license to project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [overtone "0.10.1"]
                  [leipzig "0.10.0"]]
-  :jvm-opts ^:replace [])
+  :jvm-opts ^:replace []
+  :license {:name "MIT"})
